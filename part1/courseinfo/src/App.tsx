002a68a44e8218c12eb452831c6f25d95555d3fb
Luzefiru <ian.deejay.iv@gmail.com>
2023-05-29 16:08:12 +0800

    feat: finish 1.1

---
+++ part1/courseinfo/src/App.tsx
@@ -0,0 +1,49 @@
+const Header = (props: { course: string }) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Content = (props: {
+  data: { partName: string; exerciseCount: number }[];
+}): any => {
+  return props.data.map((e) => {
+    return (
+      <p>
+        {e.partName} {e.exerciseCount}
+      </p>
+    );
+  });
+};
+
+const Total = (props: { exerciseCounts: number[] }) => {
+  return (
+    <p>
+      Number of exercises {props.exerciseCounts.reduce((acc, cur) => acc + cur)}
+    </p>
+  );
+};
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        data={[
+          { partName: part1, exerciseCount: exercises1 },
+          { partName: part2, exerciseCount: exercises2 },
+          { partName: part3, exerciseCount: exercises3 },
+        ]}
+      />
+      <Total exerciseCounts={[exercises1, exercises2, exercises3]} />
+    </div>
+  );
+};
+
+export default App;
