338d0a2de4e40092f4dfe66936b4c12d599a9b7e
Luzefiru <ian.deejay.iv@gmail.com>
2023-06-05 21:37:09 +0800

    feat: finish 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = (good - bad) / (good + neutral + bad);
+  const positive = (good / (good + neutral + bad)) * 100;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Statistics">
+      <h1>Statistics</h1>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      <div>all {all}</div>
+      <div>average {average}</div>
+      <div>positive {positive}%</div>
     </div>
   );
-}
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleIncrement = (prevValue, setValue) => {
+    return () => setValue(prevValue + 1);
+  };
+
+  return (
+    <div className="App">
+      <h1>Give Feedback</h1>
+      <div>
+        <button onClick={handleIncrement(good, setGood)}>good</button>
+        <button onClick={handleIncrement(neutral, setNeutral)}>neutral</button>
+        <button onClick={handleIncrement(bad, setBad)}>bad</button>
+      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+};
 
 export default App;
