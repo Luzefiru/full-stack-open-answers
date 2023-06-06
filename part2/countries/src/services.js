5933835395a68c26c836c52be65526adcd3d38df
Luzefiru <ian.deejay.iv@gmail.com>
2023-06-06 18:04:17 +0800

    feat: finsih 2.20

--- part2/countries/src/services.js
+++ part2/countries/src/services.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api';
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const getCountries = async () => {
   const response = await axios.get(`${baseUrl}/all`);
@@ -11,6 +12,14 @@ const getCountryData = async (countryName) => {
   return response.data;
 };
 
-const exports = { getCountries, getCountryData };
+const getCountryWeather = async (capitalName) => {
+  console.log(API_KEY);
+  const response = await axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?q=${capitalName}&appid=${API_KEY}`
+  );
+  return response.data;
+};
+
+const exports = { getCountries, getCountryData, getCountryWeather };
 
 export default exports;
