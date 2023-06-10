ff8c0c3eff433e97802e0cb819a45b6e2b85ff68
Luzefiru <ian.deejay.iv@gmail.com>
2023-06-10 20:13:52 +0800

    feat: add full crud capabilities to test project

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -35,10 +35,18 @@ app.get('/api/notes', (req, res) => {
   });
 });
 
-app.get('/api/notes/:id', (req, res) => {
-  Note.findById(req.params.id).then((note) => {
-    res.json(note);
-  });
+app.get('/api/notes/:id', (req, res, next) => {
+  Note.findById(req.params.id)
+    .then((note) => {
+      if (note) {
+        res.json(note);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 app.post('/api/notes', async (req, res) => {
@@ -59,11 +67,48 @@ app.post('/api/notes', async (req, res) => {
   });
 });
 
+app.delete('/api/notes/:id', (req, res, next) => {
+  const idToDelete = req.params.id;
+  Note.findByIdAndDelete(idToDelete)
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+app.put('/api/notes/:id', (req, res, next) => {
+  const idToUpdate = req.params.id;
+  Note.findByIdAndUpdate(idToUpdate, req.body, { new: true })
+    .then((newDoc) => {
+      if (newDoc !== null) {
+        res.status(200).json(newDoc);
+      } else {
+        res.status(404).json({ error: 'note not found' });
+      }
+    })
+    .catch((err) => next(err));
+});
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' });
 };
 
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+
+// this has to be the last loaded middleware.
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
