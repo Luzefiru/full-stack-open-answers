590a260e7bb176758d04220c6f8b1200ec2025a1
Luzefiru <ian.deejay.iv@gmail.com>
2023-06-15 21:54:24 +0800

    feat: add jwt token login route

--- part4/blog-list/app/controllers/login.controller.js
+++
@@ -1,7 +0,0 @@
-const User = require('../models/user.model');
-
-const helloWorld = () => {
-  console.log('Hello World');
-};
-
-module.exports = { helloWorld };
--- part4/blog-list/app/routes/login.routes.js
+++ part4/blog-list/app/routes/login.routes.js
@@ -1,9 +1,34 @@
 const loginRouter = require('express').Router();
-const loginController = require('../controllers/login.controller');
+const User = require('../models/user.model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-loginRouter.post('/', (req, res) => {
-  loginController.helloWorld();
-  res.status(200).send('connected route');
+loginRouter.post('/', async (req, res, next) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username }).exec();
+    const isCorrectPassword =
+      user === null ? null : await bcrypt.compare(password, user.password);
+
+    // catchers for bad input
+    if (!user) {
+      return res
+        .status(400)
+        .json({ error: 'user with that username does not exist' });
+    } else if (!isCorrectPassword) {
+      return res.status(400).json({ error: 'invalid password' });
+    }
+
+    // otherwise, validate the user and send back a token
+    const token = jwt.sign(
+      { id: user._id, username, name: user.name },
+      process.env.JWT_SECRET
+    );
+
+    res.status(200).json({ token, username, name: user.name });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = loginRouter;
