06a63cea24ba32bfd2dea229be585a5a227abf34
Luzefiru <ian.deejay.iv@gmail.com>
2023-06-16 21:31:23 +0800

    feat: refactor LoginForm and NoteForm

--- part5/notes/src/App.js
+++ part5/notes/src/App.js
@@ -12,9 +12,6 @@ const App = () => {
   const [notes, setNotes] = useState([])
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
-
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
 
   useEffect(() => {
