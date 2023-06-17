a68e4b4f579ae8f9584479c0b7efb57c7838c667
Luzefiru <ian.deejay.iv@gmail.com>
2023-06-17 14:50:18 +0800

    feat: finish 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,39 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [isShowingDetails, setIsShowingDetails] = useState(false);
+
+  const showDetails = () => {
+    setIsShowingDetails(!isShowingDetails);
+  };
+
+  const buttonText = (() => {
+    return isShowingDetails ? 'hide' : 'view';
+  })();
+
+  const details = (() => {
+    if (isShowingDetails) {
+      return (
+        <>
+          <div>{blog.url}</div>
+          <div>
+            {blog.likes} <button>like</button>
+          </div>
+          <div>{blog.author}</div>
+        </>
+      );
+    }
+  })();
+
+  return (
+    <div
+      style={{ border: '2px solid black', borderRadius: '4px', padding: '8px' }}
+    >
+      {blog.title} {blog.author}{' '}
+      <button onClick={showDetails}>{buttonText}</button>
+      {details}
+    </div>
+  );
+};
+
+export default Blog;
