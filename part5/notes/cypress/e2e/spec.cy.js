7d7b7fe8fbfefd0841253bf65e9cdb7d67d6626b
Luzefiru <ian.deejay.iv@gmail.com>
2023-06-19 14:12:47 +0800

    feat: add automated tests for NoteForm component

--- part5/notes/cypress/e2e/spec.cy.js
+++ part5/notes/cypress/e2e/spec.cy.js
@@ -1,5 +1,12 @@
 describe('Note app', function () {
   beforeEach(function () {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen',
+    }
+    cy.request('POST', 'http://localhost:3001/api/users', user)
     cy.visit('/')
   })
 
@@ -14,18 +21,18 @@ describe('Note app', function () {
   })
   it('user can login', function () {
     cy.contains('login').click()
-    cy.get('#username').type('test')
-    cy.get('#password').type('test12345')
+    cy.get('#username').type('mluukkai')
+    cy.get('#password').type('salainen')
     cy.get('#login-button').click()
 
-    cy.contains('Test Account logged in')
+    cy.contains('Matti Luukkainen logged in')
   })
 
   describe('when logged in', function () {
     beforeEach(function () {
       cy.contains('login').click()
-      cy.get('input:first').type('test')
-      cy.get('input:last').type('test12345')
+      cy.get('input:first').type('mluukkai')
+      cy.get('input:last').type('salainen')
       cy.get('#login-button').click()
     })
 
@@ -34,6 +41,21 @@ describe('Note app', function () {
       cy.get('#note-input').type('a note created by cypress')
       cy.contains('save').click()
       cy.contains('a note created by cypress')
+    })
+
+    describe('and a note exists', function () {
+      beforeEach(function () {
+        cy.contains('new note').click()
+        cy.get('input').type('another note cypress')
+        cy.contains('save').click()
+      })
+
+      it('it can be made not important', function () {
+        cy.contains('another note cypress')
+        cy.contains('make not important').click()
+
+        cy.contains('another note cypress').contains('make important')
+      })
     })
   })
 })
