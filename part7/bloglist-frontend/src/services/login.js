81f80ed00997f2d2536e4144e105e88f2048c726
Luzefiru <ian.deejay.iv@gmail.com>
2023-07-02 17:05:01 +0800

    feat: finish 7.19

--- part7/bloglist-frontend/src/services/login.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-const baseUrl = '/api/login';
-
-const login = async (username, password) => {
-  const response = await axios.post(baseUrl, { username, password });
-  return response.data; // has { token, username, name }
-};
-
-export default { login };
