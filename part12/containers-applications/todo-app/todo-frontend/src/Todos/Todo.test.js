93a9acaa2b4949c90ddc459dc74396cc4ea3a747
Luzefiru <ian.deejay.iv@gmail.com>
2023-07-13 19:34:34 +0800

    fix: test pipeline

--- part12/containers-applications/todo-app/todo-frontend/src/Todos/Todo.test.js
+++ part12/containers-applications/todo-app/todo-frontend/src/Todos/Todo.test.js
@@ -2,6 +2,6 @@ import { screen, render } from '@testing-library/react';
 import Todo from './Todo';
 
 test('it should not render content when passing it as children', () => {
-  render(<Todo>content</Todo>);
+  render(<Todo>no</Todo>);
   expect(screen.getByText('content')).toBeInTheDocument();
 });
