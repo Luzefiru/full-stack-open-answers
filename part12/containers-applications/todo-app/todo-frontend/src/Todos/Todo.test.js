5c4b46e000dd57f57eca29baa46d0e222ab9bd4c
Luzefiru <ian.deejay.iv@gmail.com>
2023-07-15 17:28:21 +0800

    feat: finish part12

--- part12/containers-applications/todo-app/todo-frontend/src/Todos/Todo.test.js
+++ part12/containers-applications/todo-app/todo-frontend/src/Todos/Todo.test.js
@@ -2,6 +2,6 @@ import { screen, render } from '@testing-library/react';
 import Todo from './Todo';
 
 test('it should not render content when passing it as children', () => {
-  render(<Todo>no</Todo>);
+  render(<Todo>content</Todo>);
   expect(screen.getByText('content')).toBeInTheDocument();
 });
